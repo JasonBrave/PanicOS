e33abba3253898f12c3eb9830602279d005ff36d
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2020-08-04 20:44:53 -0400

    [library][libcpp] Create std::array and std::pair

---
+++ library/libcpp/include/impl/move.hpp
@@ -0,0 +1,34 @@
+/*
+ * C++ std::move
+ *
+ * This file is part of PanicOS.
+ *
+ * PanicOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PanicOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PanicOS.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef _LIBCPP_IMPL_MOVE_HPP
+#define _LIBCPP_IMPL_MOVE_HPP
+
+#include <type_traits>
+
+namespace std {
+
+	template <class T>
+	constexpr typename std::remove_reference<T>::type&& move(T&& t) noexcept {
+		return static_cast<typename std::remove_reference<T>::type&&>(t);
+	}
+
+} // namespace std
+
+#endif
