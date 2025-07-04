99138985d23fcb3a52c5d564ccb3625f22cb9667
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-07-04 13:07:16 -0400

    Fix x86 compile

--- kernel/core/main.c
+++ kernel/core/main.c
@@ -240,16 +240,4 @@ static void startothers(void) {
 	}
 }
 
-// The boot page table used in entry.S and entryother.S.
-// Page directories (and page tables) must start on page boundaries,
-// hence the __aligned__ attribute.
-// PTE_PS in a page directory entry enables 4Mbyte pages.
-
-__attribute__((__aligned__(PGSIZE))) pde_t entrypgdir[NPDENTRIES] = {
-	// Map VA's [0, 4MB) to PA's [0, 4MB)
-	[0] = (0) | PTE_P | PTE_W | PTE_PS,
-	// Map VA's [KERNBASE, KERNBASE+4MB) to PA's [0, 4MB)
-	[KERNBASE >> PDXSHIFT] = (0) | PTE_P | PTE_W | PTE_PS,
-};
-
 #endif
